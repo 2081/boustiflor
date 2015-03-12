b05a8053485ff3b4baf790c0d6b9b703e864600b
Garcia Adrien <agarcia1@if501-219-10.insa-lyon.fr>
2015-03-12 20:33:34 +0100

    main-Adrien

--- src/main.cpp
+++ src/main.cpp
@@ -16,6 +16,8 @@ struct Server
 
 	Server( int s = 1, int c = 1):size(s),capacity(c)
 	{}
+
+	bool compare (Server s1, Server s2) { return (s1.score() < s2.score()); }
 };
 
 struct SubRow
@@ -27,6 +29,8 @@ struct SubRow
 	vector<Server*> servers;
 
 	SubRow(int fsize, int fx = 0):size(fsize),x(fx){}
+
+	bool compare (SubRow sb1, SubRow sb2) { return (sb1.available < sb2.available); }
 };
 
 struct Row
@@ -53,5 +57,24 @@ int main(int argc, char** argv)
 	}*/
 	Row r(2);
 
+	const int ROW_SIZE = S, ROW_NUMBER = R, SERVER_NUMBER = M;
+
+	vector<Server> servers = vector<Server>();
+
+	vector<Server>[ROW_SIZE] available_servers;
+	// vector<Row> rows;
+	vector<SubRow> subrows = vector<SubRow>();
+
+	sort(subrows.begin(), subrows.end(), SubRows::compare);
+
+	sort(servers.begin(), servers.end(), Server::compare);
+
+	for ()
+	{
+		
+	}
+
 	return 0;
 }
+
+/*************************************/
