a69b9f5b0092680b604f30486cf7a7a039ce7247
2081 <robsnicolet@hotmail.fr>
2015-03-12 19:22:23 +0100

    Server

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,17 @@
 
 using namespace std;
 
+struct Server {
+	int size, capacity;
+	double score()
+	{
+		return capacity/(double)size;
+	}
+
+	Server( int s = 1, int c = 1):size(s),capacity(c)
+	{}
+};
+
 int main(int argc, char** argv)
 {
 	int R,S,U,P,M;
