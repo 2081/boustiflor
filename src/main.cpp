d3fddb536fb93af05cda3e8975b0c9734e7ada93
2081 <robsnicolet@hotmail.fr>
2015-03-12 20:14:45 +0100

    Avancée des structs

--- src/main.cpp
+++ src/main.cpp
@@ -22,22 +22,36 @@ struct SubRow
 {
 	int size;
 	int available;
+	int x;
 	Row* row;
 	vector<Server*> servers;
+
+	SubRow(int fsize, int fx = 0):size(fsize),x(fx){}
 };
 
 struct Row
 {
 	vector<SubRow> subRows;
+
+	Row(int size)
+	{
+		subRows.push_back(SubRow(size));
+	}
+	void disableSlot(int x)
+	{
+		vector<SubRow>::iterator it = subRows.begin();
+	}
 };
 
 int main(int argc, char** argv)
 {
 	int R,S,U,P,M;
 	cin >> R >> S >> U >> P >> M;
-	cout << R << endl;
+	cerr << R << ";" << S << ";"<< U << ";"<< P << ";" <<  endl;
 	/*for( int i = 0; i < U; ++i){
 
 	}*/
+	Row r(2);
+
 	return 0;
 }
